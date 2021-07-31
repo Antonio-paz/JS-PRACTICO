49f52943b4c87727b817b969e39df85756240516
andy <antonio.paz.cambara@gmail.com>
2021-07-31 12:35:07 -0600

    agregamos comentarios para identar el codigo

--- descuentos.js
+++ descuentos.js
@@ -14,10 +14,10 @@ function BotonDescuento(){
     const valorPrecio = inputPrecio.value;
     const inputDecuento = document.getElementById("descuento");
     const valorDescuento = inputDecuento.value;
-    
 
+    //aqui se llma a la funcion para calcualar 
     const precioConDecuento = calcularDescuento(valorPrecio, valorDescuento);
-
+    //aquí escribimos el resultado de nuestra operación escribiendo html desde JS 
     const resultP = document.getElementById("resultP");
     resultP.innerText = "tu precio final es de: $" + precioConDecuento;
 }
