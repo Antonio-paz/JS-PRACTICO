22bfd383373ae34805fc33467704099821de3364
andy <antonio.paz.cambara@gmail.com>
2021-08-09 20:50:24 -0600

    mediana

--- mediana.js
+++ mediana.js
@@ -1,9 +1,22 @@
+function calcularMediaAritmetica (lista){
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, valorActual){
+            return valorAcumulado + valorActual
+        }
+    )
+     const promedio = sumaLista / lista.length
+     return promedio;
+ }
+ 
+
+
 const lista1 = [
     100,
     200,
     300,
     400,
     500,
+    600,
 ];
 
 let mediana;
@@ -21,7 +34,14 @@ function esPar (numerito){
 // calculamos la mediana con un condicional para saber si tenemos que devolver solo la mediana o la mediana con promedio
 
 if (esPar(lista1.length)){
+    const elemento1 = lista1[mitadLista1]
+    const elemento2 = lista1[mitadLista1 - 1]
 
+    const elemento1y2 = calcularMediaAritmetica ([
+        elemento1, elemento2
+    ])
+
+    mediana = elemento1y2
 }else {
     mediana = lista1[mitadLista1]
 }
